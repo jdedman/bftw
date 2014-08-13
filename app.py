e688e21db38d4bc87958cab9b78ca812d8bba44c
bftw <jdedman@cobaltopr.com>
2014-08-13 15:59:29 -0400

    02 initiating db

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
 from flask import Flask, jsonify
-
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask (__name__)
+
+# Initialize Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///soundem.db'
+db = SQLAlchemy(app)
+
+#Create class/table in DB
+class Artist(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(80), unique=True)
+	bio = db.Column(db.Text)
+
+#Constructor method
+	def __init__(self, name, bio=None): 
+		self.name = name
+
+		if bio:
+			self.bio = bio
+
+
+
 
 
 @app.route('/')
@@ -10,7 +30,7 @@ def hello():
 		'id': 1,
 		'name' : 'Mi Cancion'
 	}
-	
+
 	return jsonify(data)
 
 if __name__ == '__main__':
